25ffff0befea195d030657a2191df35fda9f1c6f
orbuluh <scuendless@gmail.com>
2024-09-28 10:19:01 +0800

    init eigen playground

--- cpp_env/proj_template/apps/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "some_lib/some_lib.h"
-
-int main() {
-  std::cout << "Hello, World!\n";
-  std::cout << "Utility value: " << some_lib::call_util() << '\n';
-  return 0;
-}
